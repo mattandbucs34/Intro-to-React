94e6339b0657bfee37801c8eebb4cbcfaddea4ac
mattandbucs34 <35737262+mattandbucs34@users.noreply.github.com>
2018-06-18 15:16:03 -0600

    add files for React-To-Do chkpt

--- react-to-do/src/App.js
+++ react-to-do/src/App.js
@@ -10,18 +10,45 @@ class App extends Component {
         { description: "Walk the Cat" , isCompleted:  true},
         { description: "Throw away the dishes" , isCompleted: false},
         { description: "Buy new dishes" , isCompleted: false}
-      ]
+      ],
+      newToDoDescription: ''
     };
   }
+
+handleChange(e) {
+  this.setState({ newToDoDescription: e.target.value });
+}
+
+handleSubmit(e) {
+  if (!this.state.newToDoDescription) { return }
+  e.preventDefault();
+  const newToDo = { description: this.state.newToDoDescription, isCompleted: false };
+  this.setState({ todos: [...this.state.todos, newToDo], newToDoDescription: '' });
+}
+
+toggleComplete(index) {
+  const todos = this.state.todos.slice();
+  const todo = todos[index];
+  todo.isCompleted = todo.isCompleted ? false : true;
+  this.setState({ todos: todos });
+}
 
   render() {
     return (
       <div className="App">
         <ul>
           { this.state.todos.map( (todo,index) => 
-           <ToDo key={ index } description = { todo.description } isCompleted = {todo.isCompleted} /> )
+           <ToDo key={ index } 
+              description = { todo.description } 
+              isCompleted = {todo.isCompleted}
+              toggleComplete = { () => this.toggleComplete(index) }
+              /> )
           }
         </ul>
+        <form onSubmit={ (e) => this.handleSubmit(e) }>
+          <input type="text" value={this.state.newToDoDescription} onChange={ (e) => {this.handleChange(e) }} />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
