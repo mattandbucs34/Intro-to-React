1df67919057302d868a7d89bc81fcd248d5c504a
mattandbucs34 <35737262+mattandbucs34@users.noreply.github.com>
2018-06-17 20:41:28 -0600

    add ToDo app

---
+++ react-to-do/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import './components/ToDo';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ul>
+          <ToDo />
+          <ToDo />
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
